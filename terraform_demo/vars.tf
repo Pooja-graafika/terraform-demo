e91dc506ceac5d05bd2a23d2fa1bb2c70ab2a51e
Pooja-graafika <84763722+Pooja-graafika@users.noreply.github.com>
2023-06-04 14:44:34 +0530

    Update vars.tf for rds db version

--- terraform_demo/vars.tf
+++ terraform_demo/vars.tf
@@ -40,7 +40,7 @@ variable "settings" {
     "database" = {
       allocated_storage   = 10            // storage in gigabytes
       engine              = "mysql"       // engine type
-      engine_version      = "8.0.27"      // engine version
+      engine_version      = "8.0.32"      // engine version
       instance_class      = "db.t2.micro" // rds instance type
       db_name             = "tutorial"    // database name
       skip_final_snapshot = true
